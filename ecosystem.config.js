0ad5eb79ae8eebba93806df48baa6700e95e6272
ocean3885 <ocean3885@gmail.com>
2026-04-15 15:37:58 +0900

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,10 @@
 module.exports = {
     apps: [{
         name: "bazi-api",
-        // 가상환경 내의 gunicorn 절대 경로를 적어주는 것이 가장 에러가 없습니다.
-        script: "./venv/bin/gunicorn",
+        script: "gunicorn", // 가상환경 활성화 상태라면 gunicorn만 써도 됩니다.
         args: "-w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8000",
-        cwd: "/var/www/manseryuk-api", // 프로젝트의 절대 경로
-        instances: 1,
-        autorestart: true,
-        watch: false,
-        env: {
-            NODE_ENV: "production",
-        }
+        exec_mode: "fork",  // gunicorn이 자체적으로 워커를 관리하므로 fork 모드가 적합합니다.
+        interpreter: "none", // Node.js로 해석하지 않도록 설정
+        cwd: "./",           // 현재 폴더를 작업 디렉토리로 설정
     }]
 }
